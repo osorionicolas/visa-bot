8b8301e7bb822379eba24714d79bddc9ab52eab7
Nicolás Osorio <osorionicolas95@gmail.com>
2022-02-16 00:37:03 -0300

    Moved to docker-compose

--- germany/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-docker stop visa-bot
-docker rm visa-bot
-docker rmi secretcolossus/visa-bot:latest
-
-docker build -t secretcolossus/visa-bot:latest .
--- spain/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker stop visa-bot
-docker rm visa-bot
-
-docker pull secretcolossus/visa-bot:latest
-
-docker run -d --init --name visa-bot secretcolossus/visa-bot:latest node -e "`cat index.js`"
